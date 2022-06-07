487146e1993ceb0c896968b3483a66120a19622d
icabris <ricabristol@gmail.com>
2022-06-08 00:17:33 +0800

    partnerships

--- frontend/routers/memo.route.js
+++ frontend/routers/memo.route.js
@@ -31,5 +31,20 @@ router.get('/dashboard', jwtMiddleware, (req, res) => {
     : console.log('404')
 });
 
+// Partners
+router.get('/partners', jwtMiddleware, (req, res) => {
+  const { roles, first_name, last_name } = req.auth;
+
+  roles.includes('Extensionist') || roles.includes('Chief')
+    ? res.render(PATH + 'partnerships', {
+      document_title: 'Partnerships',
+      active_sidebar_tab: 'Partners',
+      name: `${ first_name } ${ last_name }`,
+      role: 'Extensionist',
+      ...RENDER_OPTION_DEFAULTS
+    })
+    : console.log('404')
+});
+
 
 module.exports = router;
