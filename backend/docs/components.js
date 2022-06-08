de4f88f84febe2b51328a60d28cce6d74954be9a
pupqc-extension-2122 <extension.pupqc@gmail.com>
2022-06-08 15:57:23 +0800

    add create project schema

--- backend/docs/components.js
+++ backend/docs/components.js
@@ -21,11 +21,122 @@ module.exports = {
           }
         }
       },
+      CreateProjectBody: {
+        type: 'object',
+        properties: {
+          partner_id: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          },
+          title: {
+            type: 'string',
+            description: 'The Title of the Project',
+          },
+          target_groups: {
+            type: 'array',
+            items: {
+              type: 'string',
+            }
+          },
+          team_members: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          },
+          start_date: {
+            type: 'string',
+            format: 'date',
+            description: 'First Date of the Project'
+          },
+          end_date: {
+            type: 'string',
+            format: 'date',
+            description: 'Last Date of the Project'
+          },
+          status: {
+            type: 'string',
+            description: 'The State of the Project'
+          },
+          impact_statement: {
+            type: 'string',
+            description: 'The Impact Statement of the Project'
+          },
+          summary: {
+            type: 'string',
+            description: 'The Summary of the Project'
+          },
+          financial_requirements: {
+            type: 'array',
+            description: 'The Financial Requirements for the Project',
+            items: {
+              type: 'object',
+              properties: {
+                category: {
+                  type: 'string',
+                  description: 'The category of financial requirement'
+                },
+                items: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      budget_item: {
+                        type: 'string',
+                        description: 'The Item for the budget'
+                      },
+                      particulars: {
+                        type: 'string',
+                        description: 'The specific item'
+                      },
+                      quantity: {
+                        type: 'integer',
+                        description: 'The number of items'
+                      },
+                      estimated_cost: {
+                        type: 'integer',
+                        description: 'The total cost of the items'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          evaluation_plans: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                outcome: {
+                  type: 'string',
+                  description: 'Expected outcome'
+                },
+                collector: {
+                  type: 'string',
+                  description: 'The person to collect the evaluation'
+                },
+                data_collection_method: {
+                  type: 'string',
+                  description: 'The method for data collection'
+                },
+                frequency: {
+                  type: 'string',
+                  description: 'The frequency of data collection'
+                }
+              }
+            }
+          }
+        }
+      },
       ResponseBody: {
         type: 'object',
         properties: {
           error: {
             type: 'boolean',
+            required: true,
             description: 'Describes whether there is an error or not.',
             example: false
           },
@@ -33,6 +144,10 @@ module.exports = {
             type: 'string',
             description: 'Describes the details of the response.',
             example: 'Transaction Successful!'
+          },
+          data: {
+            type: 'object',
+            description: 'The requested resource/s from API',
           }
         }
       }
