0286398276df70c40e4bd0d00bf60f78cd722263
pupqc-extension-2122 <extension.pupqc@gmail.com>
2022-06-16 00:24:51 +0800

    fix router file for memos

--- backend/routers/memos.router.js
+++ backend/routers/memos.router.js
@@ -5,10 +5,10 @@ const {
 
 const jwtMiddleWare = require('../../utils/jwtMiddleware')
 
-router.use(jwtMiddleWare)
+// router.use(jwtMiddleWare)
 
-router.get('/:id', viewMemo)
 router.get('/datatables', dataTableMemo)
+router.get('/:id', viewMemo)
 router.put('/:id', updateMemo)
 
 module.exports = router
