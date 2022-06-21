67c4dcc337558a6c66203ff13739687378a55ae0
Jetsun Prince Torres <jetsunprincetorres@gmail.com>
2022-06-21 20:27:24 +0800

    Update evaluations.js

--- backend/sequelize/models/evaluations.js
+++ backend/sequelize/models/evaluations.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Project, {foreignKey: 'project_id', as: 'project'})
+      this.belongsTo(models.Projects, {foreignKey: 'project_id', as: 'project'})
     }
   }
   Evaluations.init({
