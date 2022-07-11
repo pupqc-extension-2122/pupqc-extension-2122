c99b84f5c442b9228c923e3e832b3c1d471ef3c8
Jetsun Prince Torres <jetsunprincetorres@gmail.com>
2022-07-11 19:36:54 +0800

    Revert "initialize controls for user management"
    This reverts commit e815ff159d26ec4676f9dc9d573231d1b5c4f646.

--- backend/sequelize/migrations/20220427081442-create-user.js
+++ backend/sequelize/migrations/20220427081442-create-user.js
@@ -37,9 +37,6 @@ module.exports = {
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE
-      },
-      deleted_at: {
-        type: Sequelize.DATE
       }
     });
   },
