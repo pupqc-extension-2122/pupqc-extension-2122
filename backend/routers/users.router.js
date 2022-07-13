68f83f06d5a3e1d2f1b2865bb1f71e4bdff284f2
pupqc-extension-2122 <extension.pupqc@gmail.com>
2022-07-13 15:34:25 +0800

    added controller/route for Users Datatable

--- backend/routers/users.router.js
+++ backend/routers/users.router.js
@@ -1,11 +1,12 @@
 let router = require('express').Router()
 const {
-  viewUser, createUser, updateUser, deleteUser, changePassword
+  viewUser, createUser, updateUser, deleteUser, changePassword, datatableUsers
 } = require('../controllers/users.controller')
 const jwtMiddleWare = require('../../utils/jwtMiddleware')
 
 router.use(jwtMiddleWare)
 
+router.get('/datatable', datatableUsers)
 router.get('/:id', viewUser)
 router.post('/create', createUser)
 router.put('/change_password', changePassword)
