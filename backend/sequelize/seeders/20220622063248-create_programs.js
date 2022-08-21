3b4cb7c092ce7cf654503dd2fdbbdc4b5e076ced
pupqc-extension-2122 <extension.pupqc@gmail.com>
2022-08-21 14:08:32 +0800

    created pkg config and fixed programs seeder

--- backend/sequelize/seeders/20220622063248-create_programs.js
+++ backend/sequelize/seeders/20220622063248-create_programs.js
@@ -23,6 +23,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('Programrs', null, {})
+    await queryInterface.bulkDelete('Programs', null, {})
   }
 };
